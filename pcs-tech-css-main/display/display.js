56cf62560d97477f48dae974993534c1db1e7692
glmsr12 <gulumsercakmak@hotmail.com>
2023-07-14 11:40:39 -0700

    "new file added"

---
+++ pcs-tech-css-main/display/display.js
@@ -0,0 +1,8 @@
+function toggleDiv() {
+    var x = document.getElementById("hiddenDiv");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
